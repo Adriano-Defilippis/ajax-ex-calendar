3b125ad7a2c39c6f899f84a3419f6dc8dc3fcbe7
Adriano-Defilippis <ciprianoadrianodefilippis@gmail.com>
2019-07-31 14:49:38 +0200

    Progetto inizzializzato con libreria JQuery e Moment.js

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+  // STEP DA SEGUIRE (non specificato nel doc):
+  //  Fare solo gennaio stampando la lista senza aggiungere le festività
+  //  Aggiungere le festività a Gennaio
+  //  Dare la possibilità di cambiare il mese
+
+  var date = moment();
+
+  console.log(date);
+
+
+
+});
