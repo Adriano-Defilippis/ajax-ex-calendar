2dfa35484f9d10be44cd542d86500eaf9f9bf3cb
Adriano-Defilippis <ciprianoadrianodefilippis@gmail.com>
2019-07-31 15:48:02 +0200

    Commenti e pulizia

--- main.js
+++ main.js
@@ -12,19 +12,17 @@ $(document).ready(function(){
   //Estrapolo dall'anno, l'oggetto del mese
   //0=Gennaio
   var month = date.month(0);
+  //Mese formattato nel formato a lettere e per intero
   var monthformattato = month.format("MMMM");
-
-  var dayOfMounth = date.date(7);
+  //Estrapolo dall'oggeto month il la data del giorno
+  var dayOfMounth = month.date(7);
+  //la formatto
   var dayOfMounthformattato = dayOfMounth.format("DD");
-
+  //DAll'oggetto del giorno del mese,
+  //formatto il giorno della settimana
   var dayOfWeek = dayOfMounth.format("dddd");
 
 
-  // var month = date.calendar();
-
-  // var day = month.format()
-
-  // console.log(date);
   console.log("Mese esaminato: ", monthformattato , month);
   console.log("Giorno del mese: ",dayOfMounthformattato,dayOfMounth);
   console.log("Giorno della settimana: ",dayOfWeek);
