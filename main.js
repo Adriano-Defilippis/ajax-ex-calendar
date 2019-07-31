93f4f7e38a130d6248a0af25a5f1aa09b49b65f7
Adriano-Defilippis <ciprianoadrianodefilippis@gmail.com>
2019-07-31 15:28:52 +0200

    Il Consol log stampa il mese in base al indice passato

--- main.js
+++ main.js
@@ -5,10 +5,24 @@ $(document).ready(function(){
   //  Aggiungere le festività a Gennaio
   //  Dare la possibilità di cambiare il mese
 
-  var date = moment();
+  //salvo in una variabile l'oggetto di ritorno
+  //dell' anno che ci interessa eseminare
+  var date = moment([2018]);
 
-  console.log(date);
+  //Estrapolo dall'anno, l'oggetto del mese
+  //0=Gennaio
+  var month = date.month(1).format("MMMM");
 
+  // var day = month.date(1);
+
+
+  // var month = date.calendar();
+
+  // var day = month.format()
+
+  // console.log(date);
+  console.log("Mese esaminato: ", month);
+  // console.log("Giorno del mese: ",day);
 
 
 });
