ca501765c0ecc4020ca5d5fffd476f6c12e834e2
Adriano-Defilippis <ciprianoadrianodefilippis@gmail.com>
2019-07-31 16:26:41 +0200

    Estrapolo il numero dei giorni nel mese e faccio un ciclo for per stamparli

--- main.js
+++ main.js
@@ -14,19 +14,39 @@ $(document).ready(function(){
   var month = date.month(0);
   //Mese formattato nel formato a lettere e per intero
   var monthformattato = month.format("MMMM");
+  //Inserisco il Mese in pagina nell'H1
+  $("#mese").text(monthformattato);
+
+  console.log("Mese esaminato: ", monthformattato , month);
+
+  var contogiorni = month.daysInMonth();
+  console.log("Giorni nel mese: ", contogiorni);
+
+
+
+
   //Estrapolo dall'oggeto month il la data del giorno
   var dayOfMounth = month.date(7);
   //la formatto
   var dayOfMounthformattato = dayOfMounth.format("DD");
+  console.log("Giorno del mese: ",dayOfMounthformattato,dayOfMounth);
+
   //DAll'oggetto del giorno del mese,
   //formatto il giorno della settimana
   var dayOfWeek = dayOfMounth.format("dddd");
-
-
-  console.log("Mese esaminato: ", monthformattato , month);
-  console.log("Giorno del mese: ",dayOfMounthformattato,dayOfMounth);
   console.log("Giorno della settimana: ",dayOfWeek);
+
+  //Ciclo chi in base ai giorni contati stampa una lista
+  //con anche il giorno della settimana relativo per il Mese in esame
+  for (var i = 1; i <= contogiorni; i++) {
+    month.date(i).format("DD");
+    $('.mounth').append(month.date(i).format("DD"),"<br>");
+    console.log(month.date(i).format("DD"));
+  }
 
 
 
 });
+
+
+// funzione che inserisce il mese restituito
